99123d25cd4b8ef5b1e42ea98166c2ebd7609fdb
Andante-Kim <dante.mjkim@gmail.com>
2024-07-19 22:55:06 +0900

    solution.py

---
+++ Mastering Algorithms and Data Structures in Python/solution.py
@@ -0,0 +1,19 @@
+def print_dict_elements(dct):
+    for key, value in dct.items():
+        print(__________)
+
+# Dictionary containing information about rockets
+rocket_info = {
+    "Falcon 1": "First privately developed liquid-fueled rocket",
+    "Atlas V": "Launch vehicle for Mars Rovers",
+    "Saturn V": "Rocket that took humans to the Moon",
+    "Space Shuttle": "First reusable spacecraft"
+}
+
+print_dict_elements(rocket_info)
+
+# Expected Output:
+# Falcon 1: First privately developed liquid-fueled rocket
+# Atlas V: Launch vehicle for Mars Rovers
+# Saturn V: Rocket that took humans to the Moon
+# Space Shuttle: First reusable spacecraft
