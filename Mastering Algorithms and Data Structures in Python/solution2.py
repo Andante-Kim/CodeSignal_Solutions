f78538119b9678df7cf51cb465955d3f9b6c9ed8
Andante-Kim <dante.mjkim@gmail.com>
2024-07-19 22:56:48 +0900

    solution2.py

---
+++ Mastering Algorithms and Data Structures in Python/solution2.py
@@ -0,0 +1,17 @@
+def print_space_elements(space_dict):
+    # TODO: Your code here
+
+# Example usage
+space_data = {
+    "Mercury": "The smallest planet",
+    "Venus": "The hottest planet",
+    "Earth": "Our home",
+    "Mars": "The Red Planet",
+    "Jupiter": "The largest planet",
+    "Saturn": "Known for its rings",
+    "Uranus": "An ice giant",
+    "Neptune": "The farthest planet",
+    "Pluto": "A dwarf planet"
+}
+
+print_space_elements(space_data)
